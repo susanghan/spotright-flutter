5ca2705ed6a7ea0f26c41f8f84e160aedb1c0fd8
algosketch <sjy_9484@naver.com>
2022-12-30 02:28:00 +0900

    FEAT: [홈 > 지도] zoom controller 삭제

--- lib/presentation/page/home/home.dart
+++ lib/presentation/page/home/home.dart
@@ -18,6 +18,7 @@ class _HomeState extends State<Home> {
       child: Scaffold(
         body: Stack(children: [
           const GoogleMap(
+            zoomControlsEnabled: false,
             myLocationButtonEnabled: true,
             myLocationEnabled: true,
             mapType: MapType.normal,
