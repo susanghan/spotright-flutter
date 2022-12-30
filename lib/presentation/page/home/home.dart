27a611c399c1f04c2cc999f4c0acc9940e2a5c78
HamBP <sjy_9484@naver.com>
2022-12-31 02:22:54 +0900

    FEAT: 현재 위치로 이동하는 함수 추가

--- lib/presentation/page/home/home.dart
+++ lib/presentation/page/home/home.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:spotright/presentation/component/sr_appbar/sr_app_bar.dart';
 import 'package:spotright/presentation/component/sr_appbar/sr_app_bar_model.dart';
 
@@ -12,20 +15,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Completer<GoogleMapController> _mapController = Completer();
+
+  // todo: 지도 내 현재 위치에 마커 찍어주기
+  void _currentLocation() async {
+    final GoogleMapController controller = await _mapController.future;
+    LocationData? currentLocation;
+    var location = Location();
+
+    try {
+      currentLocation = await location.getLocation();
+    } on Exception {
+      currentLocation = null;
+      return;
+    }
+
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      bearing: 0,
+      target: LatLng(currentLocation.latitude!, currentLocation.longitude!),
+      zoom: 17.0,
+    )));
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: _currentLocation,
+            label: Text('My Location'),
+            icon: Icon(Icons.location_on)),
         body: Stack(children: [
-          const GoogleMap(
+          GoogleMap(
             zoomControlsEnabled: false,
-            myLocationButtonEnabled: true,
             myLocationEnabled: true,
             mapType: MapType.normal,
             initialCameraPosition: CameraPosition(
               target: LatLng(37.510181246, 127.043505829),
               zoom: 14.4746,
             ),
+            onMapCreated: (GoogleMapController controller) {
+              _mapController.complete(controller);
+            },
           ),
           SrAppBar(
             srAppBarModel: SrAppBarModel(
