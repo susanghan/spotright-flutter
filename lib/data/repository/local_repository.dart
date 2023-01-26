e98c5e8f34c44ba77328e70efa9adf20b7908f86
algosketch <sjy_9484@naver.com>
2023-01-26 22:10:35 +0900

    FEAT: [로컬 저장] 로컬 저장소

---
+++ lib/data/repository/local_repository.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalRepository {
+  void save(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setString(key, value);
+  }
+
+  Future<String> fetch(String key) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(key) ?? "";
+  }
+}
