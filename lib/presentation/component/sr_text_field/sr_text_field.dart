eb5eaaf8c7cf0c768330d202391885f8e6b182fb
altpfwlzh <altpfwlzh@gmail.com>
2023-01-31 04:38:39 +0900

    Refactor : SrTextField 수정

--- lib/presentation/component/sr_text_field/sr_text_field.dart
+++ lib/presentation/component/sr_text_field/sr_text_field.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:spotright/presentation/common/colors.dart';
 
 class SrTextField extends StatelessWidget {
@@ -9,7 +10,7 @@ class SrTextField extends StatelessWidget {
       this.hint = '',
       this.onChanged,
       this.maxLines = 1,
-      this.height = 44,
+      this.height = 45,
       this.borderRadius = 22})
       : super(key: key);
 
@@ -33,8 +34,12 @@ class SrTextField extends StatelessWidget {
         textAlignVertical: TextAlignVertical.center,
         onChanged: onChanged,
         decoration: InputDecoration(
+            //아마 suffix 아이콘 사라질 듯, 나중에 허자
+            suffixIconConstraints: BoxConstraints(maxHeight: 19, maxWidth: 19, ),
             suffixIcon: suffixIcon,
-            contentPadding: const EdgeInsets.all(12),
+            //bottom 일부러 안 넣은 거임
+            contentPadding: const EdgeInsets.only(left: 16, right: 16, top: 15),
+            //Todo: 색 정정 하고 에러일 때 추가함. 수정 필요하면 해주세요.
             border: OutlineInputBorder(
                 borderRadius: BorderRadius.all(Radius.circular(borderRadius)),
                 borderSide: const BorderSide(width: 1, color: SrColors.gray1)),
