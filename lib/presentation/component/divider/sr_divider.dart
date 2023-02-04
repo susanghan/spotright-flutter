2ab60524f2d63f3c6ed3e4cfe87a46ac78d29d57
altpfwlzh <altpfwlzh@gmail.com>
2023-02-05 03:07:00 +0900

    FEAT : [SrDivider] Divider 공통 컴포넌트 생성

---
+++ lib/presentation/component/divider/sr_divider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:spotright/presentation/common/colors.dart';
+
+class SrDivider extends StatelessWidget {
+  SrDivider({Key? key, this.height = 1.5, this.color = SrColors.gray3}) : super(key: key);
+
+  double height;
+  Color color = SrColors.gray3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Divider(
+      height: height,
+      thickness: height,
+      color: color,
+    );
+  }
+}
