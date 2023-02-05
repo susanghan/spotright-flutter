3efad3fce68f4a4554d29d533881bdf042389272
algosketch <sjy_9484@naver.com>
2023-02-05 21:07:03 +0900

    FEAT: [회원] 자신 정보 가져오기

--- lib/data/user/user_repository.dart
+++ lib/data/user/user_repository.dart
@@ -1,18 +1,35 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
 import 'package:jwt_decode/jwt_decode.dart';
 import 'package:logger/logger.dart';
 import 'package:spotright/common/network_client.dart';
+import 'package:spotright/data/model/response_wrapper.dart';
 import 'package:spotright/data/user/user_response.dart';
 
 class UserRepository {
+  final String refreshTokenPath = "/member/token/renew";
+  final String getUserInfoPath = "/member";
+
   NetworkClient networkClient = Get.find();
   Logger logger = Get.find();
   UserResponse? userResponse;
   String? accessToken;
   String? refreshToken;
 
+  Future<void> fetchMyInfo() async {
+    if(userResponse == null) return;
+
+    await verifyAndRefreshToken();
+    Map<String, String> requestHeader = {"authorization": accessToken!};
+    var res = await networkClient.request(path: "$getUserInfoPath/${userResponse!.memberId}", headers: requestHeader);
+    ResponseWrapper responseWrapper = ResponseWrapper.fromJson(jsonDecode(res.body));
+    UserResponse newUserResponse = UserResponse.fromJson(responseWrapper.data!);
+    userResponse = newUserResponse;
+  }
+
   Future<void> verifyAndRefreshToken() async {
-    if(isValidToken(afterMinutes: 10)) await refreshLogin();
+    if(!isValidToken(afterMinutes: 10)) await refreshLogin();
   }
 
   bool isValidToken({int afterMinutes = 0}) {
@@ -30,10 +47,10 @@ class UserRepository {
   Future<void> refreshLogin() async {
     if(refreshToken == null) return;
 
-    Map<String, String> requestHeader = {"Authorization": refreshToken!};
-    Response res = await networkClient.request(path: "/member/token/renew", headers: requestHeader) as Response<void>;
+    Map<String, String> requestHeader = {"authorization": refreshToken!};
+    var res = await networkClient.request(path: refreshTokenPath, headers: requestHeader);
     Map<String, String>? headers = res.headers;
-    String? auth = headers?["Authorization"];
+    String? auth = headers["authorization"];
 
     // todo : 실패 케이스 처리
     if(auth == null) return;
@@ -43,6 +60,5 @@ class UserRepository {
     String newRefreshToken = splitTokens[1].split(":")[1].replaceAll(" ", "");
     accessToken = "Bearer $newAccessToken";
     refreshToken = "Bearer $newRefreshToken";
-    logger.d("refreshLogin $newAccessToken, $newRefreshToken");
   }
 }
