33d5de602ec30959ac560f1b94012c4123d90ebf
algosketch <sjy_9484@naver.com>
2023-02-05 21:06:41 +0900

    FIX: [네트워크] ResponseWrapper 수정

--- lib/data/model/response_wrapper.dart
+++ lib/data/model/response_wrapper.dart
@@ -1,24 +1,14 @@
-class ResponseWrapper<T extends ResponseConverter> {
+class ResponseWrapper {
   String? responseCode;
   String? responseMessage;
-  ResponseConverter? data;
+  Map<String, dynamic>? data;
 
   ResponseWrapper({this.responseCode, this.responseMessage, this.data});
 
   ResponseWrapper.fromJson(Map<String, dynamic> json) {
     responseCode = json['responseCode'];
     responseMessage = json['responseMessage'];
-    data = json['data'] != null ? ResponseConverter.fromJson(json['data']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['responseCode'] = this.responseCode;
-    data['responseMessage'] = this.responseMessage;
-    if (this.data != null) {
-      data['data'] = this.data!.toJson();
-    }
-    return data;
+    data = json['data'];
   }
 }
 
