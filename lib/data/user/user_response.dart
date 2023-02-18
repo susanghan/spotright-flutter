65b52cdbab01009db1dde4a68175dce2edeefeb0
algosketch <sjy_9484@naver.com>
2023-02-19 01:02:44 +0900

    FIX: [DTO] UserResponse 수정

--- lib/data/user/user_response.dart
+++ lib/data/user/user_response.dart
@@ -1,10 +1,7 @@
-import 'dart:ffi';
-
 import 'package:spotright/data/model/response_wrapper.dart';
-import 'package:spotright/data/user/user_response.dart';
 
 class UserResponse implements ResponseConverter {
-  int? memberId;
+  int memberId;
   int? followingsCnt;
   int? followersCnt;
   int? blockedsCnt;
@@ -17,48 +14,47 @@ class UserResponse implements ResponseConverter {
   String? nickname;
 
   UserResponse(
-      {this.memberId,
-        this.followingsCnt,
-        this.followersCnt,
-        this.blockedsCnt,
-        this.memberPhoto,
-        this.memberSpotsCnt,
-        this.spotrightId,
-        this.email,
-        this.gender,
-        this.birthdate,
-        this.nickname});
+      {required this.memberId,
+      this.followingsCnt,
+      this.followersCnt,
+      this.blockedsCnt,
+      this.memberPhoto,
+      this.memberSpotsCnt,
+      this.spotrightId,
+      this.email,
+      this.gender,
+      this.birthdate,
+      this.nickname});
 
-  UserResponse.fromJson(Map<String, dynamic> json) {
-    memberId = json['memberId'];
-    followingsCnt = json['followingsCnt'];
-    followersCnt = json['followersCnt'];
-    blockedsCnt = json['blockedsCnt'];
-    memberPhoto = json['memberPhoto'] != null
-        ? MemberPhoto.fromJson(json['memberPhoto'])
-        : null;
-    memberSpotsCnt = json['memberSpotsCnt'];
-    spotrightId = json['spotrightId'];
-    email = json['email'];
-    gender = json['gender'];
-    birthdate = json['birthdate'];
-    nickname = json['nickname'];
-  }
+  UserResponse.fromJson(Map<String, dynamic> json)
+      : memberId = json['memberId'],
+        followingsCnt = json['followingsCnt'],
+        followersCnt = json['followersCnt'],
+        blockedsCnt = json['blockedsCnt'],
+        memberPhoto = json['memberPhoto'] != null
+            ? MemberPhoto.fromJson(json['memberPhoto'])
+            : null,
+        memberSpotsCnt = json['memberSpotsCnt'],
+        spotrightId = json['spotrightId'],
+        email = json['email'],
+        gender = json['gender'],
+        birthdate = json['birthdate'],
+        nickname = json['nickname'];
 
   @override
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['memberId'] = this.memberId;
-    data['followingsCnt'] = this.followingsCnt;
-    data['followersCnt'] = this.followersCnt;
-    data['blockedsCnt'] = this.blockedsCnt;
-    data['memberPhoto'] = this.memberPhoto;
-    data['memberSpotsCnt'] = this.memberSpotsCnt;
-    data['spotrightId'] = this.spotrightId;
-    data['email'] = this.email;
-    data['gender'] = this.gender;
-    data['birthdate'] = this.birthdate;
-    data['nickname'] = this.nickname;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['memberId'] = memberId;
+    data['followingsCnt'] = followingsCnt;
+    data['followersCnt'] = followersCnt;
+    data['blockedsCnt'] = blockedsCnt;
+    data['memberPhoto'] = memberPhoto;
+    data['memberSpotsCnt'] = memberSpotsCnt;
+    data['spotrightId'] = spotrightId;
+    data['email'] = email;
+    data['gender'] = gender;
+    data['birthdate'] = birthdate;
+    data['nickname'] = nickname;
     return data;
   }
 }
@@ -75,9 +71,9 @@ class MemberPhoto {
   }
 
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['memberPhotoId'] = this.memberPhotoId;
-    data['photoUrl'] = this.photoUrl;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['memberPhotoId'] = memberPhotoId;
+    data['photoUrl'] = photoUrl;
     return data;
   }
 }
