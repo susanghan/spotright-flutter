4b099a0f6a375023bf73d6ed9826ba6941c27038
algosketch <sjy_9484@naver.com>
2023-02-19 22:20:22 +0900

    FEAT: [신고] 신고 욥션

---
+++ lib/presentation/component/appbars/report_options.dart
@@ -0,0 +1,9 @@
+class ReportOptions {
+  static Map<String, String> options = {
+    "SPOTRIGHT_ID": "아이디",
+    "NICKNAME": "닉네임",
+    "PROFILE_PHOTO": "프로필 사진",
+    "PROMOTION": "홍보성",
+    "ETC": "기타",
+  };
+}
