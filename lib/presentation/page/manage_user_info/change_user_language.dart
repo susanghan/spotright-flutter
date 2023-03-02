e31239573d2d6dc1cccacd9d0f1828b0f1b0637c
algosketch <sjy_9484@naver.com>
2023-03-02 23:12:28 +0900

    REFACTOR: [마이페이지] 언어 설정 코드 간결화

--- lib/presentation/page/manage_user_info/change_user_language.dart
+++ lib/presentation/page/manage_user_info/change_user_language.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import '../../common/colors.dart';
 import '../../component/appbars/default_app_bar.dart';
 import '../../component/divider/sr_divider.dart';
@@ -20,32 +21,29 @@ class ChangeUserLanguage extends StatelessWidget {
       body: Column(
         children: [
           SrDivider(),
-          ..._ListText(listText: "한국어", action: () {}),
+          _ListText(listText: "한국어", action: () {}),
           SrDivider(),
-          ..._ListText(listText: "English", action: () {}),
+          _ListText(listText: "English", action: () {}),
           SrDivider(),
         ],
       ),
     ));
   }
 
-  List<Widget> _ListText(
-      {required String listText, required Function() action}) {
-    return [
-      InkWell(
-        onTap: action,
-        child: Container(
-            height: 50,
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.only(left: 32),
-            child: Text(
-              listText,
-              style: const TextStyle(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 15,
-                  color: SrColors.black),
-            )),
-      )
-    ];
+  Widget _ListText({required String listText, required Function() action}) {
+    return InkWell(
+      onTap: action,
+      child: Container(
+          height: 50,
+          alignment: Alignment.centerLeft,
+          padding: const EdgeInsets.only(left: 32),
+          child: Text(
+            listText,
+            style: const TextStyle(
+                fontWeight: FontWeight.w300,
+                fontSize: 15,
+                color: SrColors.black),
+          )),
+    );
   }
 }
