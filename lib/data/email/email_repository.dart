092352dad2941a31bc844b46937445756800f44c
HamBP <sjy_9484@naver.com>
2023-03-07 13:29:03 +0900

    FEAT: [회원가입] email repository 추가

---
+++ lib/data/email/email_repository.dart
@@ -0,0 +1,30 @@
+import 'package:get/get.dart';
+
+import '../../common/network_client.dart';
+
+class EmailRepository {
+  NetworkClient networkClient = Get.find();
+
+  final String _sendMailPath = "/member/email";
+  final String _verifyEmailPath = "/member/email";
+
+  Future<void> sendMail(String email) async {
+    await networkClient.request(method: Http.post, path: _sendMailPath, body: {
+      "email": email,
+      "language": "KR"
+    });
+  }
+
+  Future<bool> verifyEmail(String code) async {
+    var res = await networkClient.request(method: Http.post, path: _verifyEmailPath, body: {
+      "email": "",
+      "verificationCode": code
+    });
+
+    if(res.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
