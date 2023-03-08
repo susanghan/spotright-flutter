af2b16e29b0c9a3adc2caf76f04460b00ab17041
algosketch <sjy_9484@naver.com>
2023-03-09 00:33:52 +0900

    FEAT: 스팟 업로드용 api 추가

--- lib/data/file/file_repository.dart
+++ lib/data/file/file_repository.dart
@@ -5,6 +5,7 @@ import 'package:http_parser/http_parser.dart';
 
 //프로필 이미지나, 스팟 이미지 파일 업로드 할 때 사용하는 것
 class FileRepository {
+  Dio dio = Dio();
   final String _postProfileFilePath = "/member/photo";
   final String _postSpotFilePath = "/member/spot/files";
 
@@ -16,18 +17,19 @@ class FileRepository {
     }
 
     FormData formData = FormData.fromMap({'photo': MultipartFile.fromFileSync(photoPath!, contentType: MediaType("image", "jpeg"))});
-    return await updateUserProfile(formData);
+    return uploadImage(formData, _postProfileFilePath);
   }
 
-  Future<void> postSpotFile() async {
-    await networkClient.request(method: Http.post, path: _postSpotFilePath);
+  Future<bool> uploadSpotImages(List<String> photoPaths, int spotId) async {
+    List<MultipartFile> files = photoPaths.map((photoPath) => MultipartFile.fromFileSync(photoPath, contentType: MediaType("image", "jpeg"))).toList();
+    FormData formData = FormData.fromMap({'photos': files, 'memberSpotId': spotId});
+    return uploadImage(formData, _postSpotFilePath);
   }
 
-  Future<bool> updateUserProfile(FormData formData) async {
-    var dio = Dio();
+  Future<bool> uploadImage(FormData formData, String path) async {
     dio.options.headers = {"authorization": networkClient.accessToken};
     dio.options.contentType = 'multipart/form-data';
-    var response = await dio.post("https://${networkClient.baseUrl}${networkClient.prefix}$_postProfileFilePath", data: formData);
+    var response = await dio.post("https://${networkClient.baseUrl}${networkClient.prefix}$path", data: formData);
     return (response.statusCode! / 100) == 2;
   }
 }
