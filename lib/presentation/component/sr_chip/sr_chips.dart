88d6686edf543a5f520265388e56b4a5c533c630
altpfwlzh <altpfwlzh@gmail.com>
2023-03-18 13:01:15 +0900

    FEAT : [칩] 높이 변수로 받을 수 잇게 수정 및 목록 보기 편집 margin 수정

--- lib/presentation/component/sr_chip/sr_chips.dart
+++ lib/presentation/component/sr_chip/sr_chips.dart
@@ -5,11 +5,12 @@ import 'package:spotright/presentation/component/sr_chip/sr_chip.dart';
 
 class SrChips extends StatelessWidget {
   SrChips(
-      {Key? key, this.onCategorySelected, this.selectedCategories = const {}})
+      {Key? key, this.onCategorySelected, this.selectedCategories = const {}, this.height})
       : super(key: key);
 
   Function(String, bool)? onCategorySelected;
   Set<String> selectedCategories;
+  double? height;
 
   List<String> chipNames = ["전체", ...SpotCategory.mainCategory];
 
@@ -17,7 +18,7 @@ class SrChips extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         width: double.infinity,
-        height: 40,
+        height: height ?? 40,
         margin: EdgeInsets.only(top: 10),
         child: ListView(
           padding: EdgeInsets.only(left: 16),
