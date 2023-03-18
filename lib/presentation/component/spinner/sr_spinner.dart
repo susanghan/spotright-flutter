51fb8314639f5bcd9191575f58e94106acacdd60
altpfwlzh <altpfwlzh@gmail.com>
2023-03-18 16:11:11 +0900

    FEAT : [birthdayDialog] 디자인 규격 맞춰둠

--- lib/presentation/component/spinner/sr_spinner.dart
+++ lib/presentation/component/spinner/sr_spinner.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotright/presentation/common/colors.dart';
+import 'package:spotright/presentation/common/typography.dart';
 
 class SrSpinner extends StatelessWidget {
   SrSpinner({Key? key, required this.defaultValue, required this.list, required this.onChanged}) : super(key: key);
@@ -7,14 +8,15 @@ class SrSpinner extends StatelessWidget {
   final List<String> list;
   final Function(String current) onChanged;
   String defaultValue;
+  int? seletedValue;
 
   @override
   Widget build(BuildContext context) {
-    PageController controller = PageController(initialPage: list.indexOf(defaultValue), viewportFraction: 0.3);
+    PageController controller = PageController(initialPage: list.indexOf(defaultValue), viewportFraction: 0.4);
 
     return Container(
       height: 80,
-      width: 54,
+      width: 53,
       child: Stack(
         alignment: Alignment.center,
           children: [
@@ -24,6 +26,7 @@ class SrSpinner extends StatelessWidget {
           controller: controller,
           itemBuilder: (_, i) => _buildItem(list[i]),
           onPageChanged: (page) {
+            seletedValue = page;
             onChanged(list[page]);
           },
         ),
@@ -51,6 +54,6 @@ class SrSpinner extends StatelessWidget {
     return Container(
       alignment: Alignment.center,
       padding: EdgeInsets.symmetric(vertical: 4),
-        child: Text(text));
+        child: Text(text, style: SrTypography.body2semi.copy(color: SrColors.gray2),));
   }
 }
