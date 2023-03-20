73a5b65c3640ad73db070e449b7f3c8d0662089a
altpfwlzh <altpfwlzh@gmail.com>
2023-03-20 18:07:28 +0900

    FEAT : [생일 다이어로그] 선택되고 나서 글씨 컬러 블랙

--- lib/presentation/component/dialog/birthday_dialog.dart
+++ lib/presentation/component/dialog/birthday_dialog.dart
@@ -37,7 +37,7 @@ class _BirthdayDialogState extends State<BirthdayDialog> {
         height: 260,
         padding: EdgeInsets.only(top: 32),
         child: Column(children: [
-          Padding(padding: EdgeInsets.only(bottom: 24), child: Text("생년월일을 입력하세요", style: SrTypography.body2semi,)),
+          Padding(padding: EdgeInsets.only(bottom: 24), child: Text("생년월일을 입력하세요", style: SrTypography.body2semi.copy(color: SrColors.black))),
           SizedBox(
             width: 202,
             child: Row(
